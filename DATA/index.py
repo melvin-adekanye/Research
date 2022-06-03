50e739e777439828b11e0fb8a0120a805d02e610
I AM <1baller4god@gmail.com>
2022-06-03 10:39:12 -0600

    Updated the DATA folder

--- DATA/index.py
+++ DATA/index.py
@@ -23,7 +23,7 @@ os.mkdir(path2)
 # Should get chromatic numbers between 2 and the number of vertices.
 
 min_vertices = 3
-max_vertices = 10
+max_vertices = 9
 
 # Set start time
 start_time = time.time()
@@ -36,38 +36,46 @@ vertices = []
 def save():
 
     for graph in critical_graphs:
-        
+
+        string = graph[0]
+        chromatic_number = graph[1]
+        vertice = graph[2] 
+
+        graph = Graph(string)
+        graph6_string = graph.graph6_string()
+
         # Store this graph in the grpahs folder
         f = open(f'./critical_graphs/order{vertice}_chi{chromatic_number}.txt', "a")
 
-        # Save the graphs in graph6 format
+        # Write to file
+        f.write(f'{graph6_string}\n')
+    
+        # CLose the file
+        # f.close()
+
+
+    for graph in not_critical:
+
+        string = graph[0]
+        chromatic_number = graph[1]
+        vertice = graph[2] 
+
+        graph = Graph(string)
         graph6_string = graph.graph6_string()
 
-        # Get its chromatic number
-        chromatic_number = graph.chromatic_number()
+        # Store this graph in the grpahs folder
+        f = open(f'./not_critical_graphs/order{vertice}_chi{chromatic_number}.txt', "a")
 
         # Write to file
         f.write(f'{graph6_string}\n')
 
         # CLose the file
-        f.close()
-        
-        
-        # Write to file
-        f.write(f'{graph}\n')
-
-    for graph in not_critical:
-        
-        # Write to file
-        g.write(f'{graph}\n')
-
-
-    # CLose the file
-    f.close()
-    g.close()
+        # f.close()
 
 # Loop through all graphs with 3 - 7 vertices
 for vertice in range(min_vertices, max_vertices + 1):
+
+    print(f'Vertex #{vertice}')
 
     # Create the query
     query = "%d -c" % (vertice)
@@ -77,74 +85,74 @@ for vertice in range(min_vertices, max_vertices + 1):
         graphs.nauty_geng(query)
     )
 
-    # Append the vertice to the vertices
-    vertices.append(vertice)
+    test = 0
 
-# Merge the vertices along with the 
-graph_list = [[x,y,z] for x,y,z in zip(A,B,C)]
+    # FOr every graph_string in the graphs
+    for string in graph_list:
 
-# FOr every graph_string in the graphs
-for string in graph_list:
+        # Create the graph using the string
+        graph = Graph(string)
+        original_graph = Graph(string)
 
-    # Create the graph using the string
-    graph = Graph(string)
-    original_graph = Graph(string)
+        # A flag to check if a graph is critical
+        is_critical = True
 
-    # A flag to check if a graph is critical
-    is_critical = True
+        # Get its chromatic number
+        chromatic_number = original_graph.chromatic_number()
 
-    # Get its chromatic number
-    chromatic_number = original_graph.chromatic_number()
+        # Degrees
+        degrees = original_graph.degree()
 
-    # Degrees
-    degrees = original_graph.degree()
+        # Create the degrees and index
+        indice_n_degree = [[index, degree]
+                        for index, degree in enumerate(degrees)]
 
-    # Create the degrees and index
-    indice_n_degree = [[index, degree]
-                       for index, degree in enumerate(degrees)]
+        # Delete highest degree vertice first
+        delete_vertice_with_highest_degree_first = False
 
-    # Delete highest degree vertice first
-    delete_vertice_with_highest_degree_first = False
+        # Sort the indice_n_degree by the degree value
+        indice_n_degree.sort(
+            key=lambda x: x[1], reverse=delete_vertice_with_highest_degree_first)
 
-    # Sort the indice_n_degree by the degree value
-    indice_n_degree.sort(
-        key=lambda x: x[1], reverse=delete_vertice_with_highest_degree_first)
-
-    # Define min degree
-    min_degree = indice_n_degree[0][0]
-    
-    # Check if the minimum degree is smaller than k - 1. If so, it is not critical
-    if min_degree > (chromatic_number - 1):
+        # Define min degree
+        min_degree = indice_n_degree[0][0]
         
-        # Iterate through every vertice in graphs vertices
-        for (index, degree) in indice_n_degree:
+        # Check if the minimum degree is smaller than k - 1. If so, it is not critical
+        if min_degree > (chromatic_number - 1):
+            
+            # Iterate through every vertice in graphs vertices
+            for (index, degree) in indice_n_degree:
 
-            # Remove the vertex at index
-            graph.delete_vertex(index)
+                # Remove the vertex at index
+                graph.delete_vertex(index)
 
-            # Get the new chromatic number
-            new_chromatic_number = graph.chromatic_number()
+                # Get the new chromatic number
+                new_chromatic_number = graph.chromatic_number()
 
-            # Reset the graph to replace deleted vertex
-            graph = Graph(original_graph)
+                # Reset the graph to replace deleted vertex
+                graph = Graph(original_graph)
 
-            # If the new chromatic number is greater or equal to the current one. Is critical is false
-            if new_chromatic_number >= chromatic_number:
+                # If the new chromatic number is greater or equal to the current one. Is critical is false
+                if new_chromatic_number >= chromatic_number:
 
-                # Is critical is false
-                is_critical = False
+                    # Is critical is false
+                    is_critical = False
 
-                break
+                    break
+        
+        test += 1
 
-    # If the graph is critical
-    if is_critical == True:
+        print(f'{test} / {len(graph_list)}')
+        
+        # If the graph is critical
+        if is_critical == True:
 
-        # Push it to the critical_graphs list
-        critical_graphs.append([string, chromatic_number, ])
+            # Push it to the critical_graphs list
+            critical_graphs.append([string, chromatic_number, vertice])
 
-    else:
+        else:
 
-        not_critical.append(string)
+            not_critical.append([string, chromatic_number, vertice])
 
 
 # Set end time (not a Prophet tho)
