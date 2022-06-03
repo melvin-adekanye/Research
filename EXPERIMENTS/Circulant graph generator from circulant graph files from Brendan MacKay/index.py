a20c43ab31e9b742c4a7481a192bd80108141a8d
Glibby Globber <1baller4god@gmail.com>
2022-06-03 09:01:48 -0600

    Push

--- EXPERIMENTS/Circulant graph generator from circulant graph files from Brendan MacKay/index.py
+++ EXPERIMENTS/Circulant graph generator from circulant graph files from Brendan MacKay/index.py
@@ -1,1 +1,26 @@
 from sage.all import *
+
+
+
+
+# Create the circulant graph
+def circulant_graph(number_of_vertices):
+    
+    connections = []
+    
+    for vertex in range(number_of_vertices):
+
+        for new_vertex in range(number_of_vertices - 1):
+            
+            if vertex != new_vertex:
+            
+                connection = [vertex, new_vertex]
+                
+                connections.append(connection)
+        
+    return connections
+
+cc = circulant_graph(12)
+print(cc)
+
+Graph(cc).show()
