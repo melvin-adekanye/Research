33664234ae414ba7d421004886674d8c082e1dee
melvin12345678910 ~ melvin123456789 was taken <melvin.adekanye@intelius.ai>
2022-07-11 09:55:48 -0600

    Some Critical graphs with params

--- EXPERIMENTS/Circulant graph generator from circulant graph files from Brendan MacKay/index.py
+++ EXPERIMENTS/Circulant graph generator from circulant graph files from Brendan MacKay/index.py
@@ -2,32 +2,33 @@ import os
 import time
 import shutil
 from sage.graphs.graph_coloring import vertex_coloring
+from sage import *
 
 # The selected chromatic number
-k = 5
+k = int(input('k='))
 
 # The raw and regular graphs
 raw_graphs = []
 graphs = []
 
 min_vertices = 5  # circ-5 graphs
-max_vertices = 30  # circ-50 graphs
+max_vertices = 50  # circ-50 graphs
 
 # Ask to delete old graph - X data
 x = input(f'delete "/graphs - {k}" (y/n): ')
 
+# Create the path for the graphs to be stored
+graph_path = f'{os.getcwd()}/graphs - {k}'
+source_path = f'{os.getcwd()}/raw graphs'
+
+default_graph6_string_path = f'{graph_path}/all graphs'
+default_params_path = f'{graph_path}/all graphs params'
+
+critical_graph6_string_path = f'{graph_path}/critical graphs'
+critical_params_path = f'{graph_path}/critical graphs params'
+
 # If yes
 if x == 'y':
-
-    # Create the path for the graphs to be stored
-    graph_path = f'{os.getcwd()}/graphs - {k}'
-    raw_path = f'{os.getcwd()}/raw graphs'
-
-    path = f'{graph_path}/graph6_string'
-    grapH_raw_path = f'{graph_path}/raw'
-
-    critical_path = f'{graph_path}/critical graphs'
-    critical_raw_path = f'{graph_path}/critical graphs raw'
 
     # Remove graphs
     shutil.rmtree(graph_path, ignore_errors=True)
@@ -39,84 +40,115 @@ if x == 'y':
     os.mkdir(graph_path)
 
     # Create the sub paths
-    os.mkdir(path)
-    os.mkdir(raw_path)
-    os.mkdir(critical_path)
-    os.mkdir(critical_raw_path)
+    os.mkdir(default_graph6_string_path)
+    os.mkdir(default_params_path)
+    os.mkdir(critical_graph6_string_path)
+    os.mkdir(critical_params_path)
 
-# Critical check. Takes in the graph6_string, graph order and the chromatic number
+# Critical check. Takes in the graph6_string, graph raw string and the chromatic number
 
 
-def critical_check(string, order, chromatic_number):
+def critical_check(graph, chromatic_number):
 
-    # If the chromatic number is not k
-    if chromatic_number != k:
+    # Set the original graph before messing with it
+    original_graph = Graph(graph)
 
-        # Return not critical
-        return False
+    # if there is no cn
+    if chromatic_number == None:
 
-    # Create the graph using the string
-    graph = Graph(string)
-    original_graph = Graph(string)
+        # Get it from the graph
+        chromatic_number = original_graph.chromatic_number()
 
-    # A flag to check if a graph is critical
-    is_critical = True
+    # Only if the CN is K
+    if chromatic_number == k:
 
-    # Iterate through every vertice in graphs vertices
-    for index in range(order):
+        # Get the order of the graph
+        order = original_graph.order()
 
-        # print out the vertex being deleted
-        print(f'. . . Deleting Vertice-{index} of {order}')
+        # A flag to check if a graph is critical
+        is_critical = True
 
-        # Remove the vertex at index
-        graph.delete_vertex(index)
+        # Iterate through every vertice in graphs vertices
+        for index in range(order):
 
-        # After deleting a vertex, can we color this graph with less colors. (Faster than re-caculating the chormatic number)
-        if vertex_coloring(graph, k=k-1, value_only=True) == False:
+            # print out the vertex being deleted
+            print(f'. . . Deleting Vertice-{index} of {order}')
 
-            # Is critical is false
-            is_critical = False
+            # Remove the vertex at index
+            graph.delete_vertex(index)
 
-            # Not critical, lets break the loop
-            break
+            # After deleting a vertex, can we color this graph with less colors. (Faster than re-caculating the chormatic number)
+            if vertex_coloring(graph, k=k-1, value_only=True) == False:
 
-    # Reset the graph to replace deleted vertex
-    graph = Graph(original_graph)
+                # Is critical is false
+                is_critical = False
 
-    # Return is critical flag
-    return is_critical
+                # Not critical, lets break the loop
+                break
+
+        # Reset the graph to replace deleted vertex
+        graph = Graph(original_graph)
+
+        # Return is_critical
+        return is_critical
+
+    # Return not critical to k value
+    return False
+
 
 # Save the graph in general
 
 
-def save(string, raw_string, order, chromatic_number):
+def save(type, string, raw_string, order, chromatic_number):
 
-    # Store this graph in the grpahs folder
-    f = open(f'{path}/circ{order}_chi{chromatic_number}.txt', "a")
-    e = open(f'{grapH_raw_path}/circ{order}_chi{chromatic_number}.txt', "a")
+    if type == 'default':
 
-    # Write to file
-    f.write(f'{string}\n')
-    e.write(f'{raw_string}\n')
+        # Store this graph in the grpahs folder
+        f = open(
+            f'{default_graph6_string_path}/circ{order}_chi{chromatic_number}.txt', "a")
+        e = open(
+            f'{default_params_path}/circ{order}_chi{chromatic_number}_params.txt', "a")
 
-    # CLose the file save
-    f.close()
-    e.close()
+        # Write to file
+        f.write(f'{string}\n')
+        e.write(f'{raw_string}\n')
+
+        # CLose the file save
+        f.close()
+        e.close()
+
+    elif type == 'critical':
+
+        # Store this graph in the grpahs folder
+        f = open(f'{critical_graph6_string_path}/circ{order}_crit{k}.txt', "a")
+        e = open(f'{critical_params_path}/circ{order}_crit{k}_params.txt', "a")
+
+        # Write to file
+        f.write(f'{string}\n')
+        e.write(f'{raw_string}\n')
+
+        # CLose the file
+        f.close()
+        e.close()
 
 
-def save_critical(string, raw_string, order):
+# Convert array to string
+def convert_array_to_string(array):
 
-    # Store this graph in the grpahs folder
-    f = open(f'{critical_path}/circ{order}_crit{k}.txt', "a")
-    e = open(f'{critical_raw_path}/circ{order}_crit{k}.txt', "a")
+    total_string = ''
 
-    # Write to file
-    f.write(f'{string}\n')
-    e.write(f'{raw_string}\n')
+    # traverse in the string
+    for (i, string) in enumerate(array):
 
-    # CLose the file
-    f.close()
-    e.close()
+        total_string += str(string)
+
+        # Don't add space after last character
+        if i < len(array)-1:
+
+            total_string += ' '
+
+    # Return the total string
+    return total_string
 
 # Attain the graph string from circulant graph
 
@@ -135,62 +167,83 @@ def circulant(n, L):
 
                     E.append({i, j})
 
-    graph = Graph(E)
-
-    graph_string = graph.graph6_string()
-
-    return graph, graph_string
+    # Return the graph
+    return E
 
 
+print('. . . Gathering graph data files from raw graphs folder')
 # Loop through all graphs with {min_vertices} to {max_vertices}
-for vertice in range(min_vertices, max_vertices + 1):
+for order in range(min_vertices, max_vertices + 1):
 
     # For every grpah in the list (2 is the minimum chromatic number in raw graphs)
-    for circulant_number in range(0, max_vertices):
+    for circulant_number in range(max_vertices):
 
-        # Defien the path of the graph
-        graph_path = f'{grapH_raw_path}/circ{vertice}.{circulant_number}.txt'
+        # Defien a  path to the graph (may not actually exist)
+        path = f'{source_path}/circ{order}.{circulant_number}.txt'
 
+        # Try: If the graph_path does exist
         try:
+
             # Open up and read the file graph
-            f = open(graph_path, "r")
+            f = open(path, "r")
 
             # FOr every line in the file
             for line in f:
 
                 # Attain the raw graph numbers
-                results = line.split(None)
-                results = list(map(int, results))
+                string = line.split(None)
 
-                # Create a list of nauty_geng connected graphs
-                raw_graphs.append([vertice, results, circulant_number])  # add only first word
+                string = list(map(int, string))
+
+                # Append a list to the raw graph
+                raw_graphs.append([order, string])
 
             # CLose the file
             f.close()
 
         except:
 
+            # If the graph path doesn't exist skip
             pass
 
+print('. . . Analyzing graph data')
 # For all the raw graphs
-for (index, graph_data) in enumerate(raw_graphs):
+for (index, data) in enumerate(raw_graphs):
 
-    # Define the raw_graph_string
-    raw_graph_string = f"{graph_data[0]} {' '.join([str(x) for x in graph_data[1]])}"
+    # Define the order
+    order = data[0]
 
-    print(f'{index} out of {len(raw_graphs)} ~ {graph_data}')
+    # Define the graph raw string (5 7 10 12)
+    graph_raw_string = data[1]
 
-    # Get the graphs chromatic number and save it to a new file
-    graph, graph_string = circulant(*graph_data)
+    # Remove array props from the graph_raw_string
+    graph6_raw_array_to_string = convert_array_to_string(graph_raw_string)
 
-    # get the order of the graph
-    order = graph.order()
+    # graph = circulant(first_elem, remaining_elems)
+    E = circulant(order, graph_raw_string)
+
+    # Create graphs and get details
+    graph = Graph(E)
+    graph6_string = graph.graph6_string()
+    chromatic_number = graph.chromatic_number()
 
     # Check if this graph is critical
-    if critical_check(graph_string, order, chromatic_number) == True:
+    is_critical = critical_check(graph, chromatic_number)
 
-        # Save this graph
-        save_critical(graph_string, raw_graph_string, order)
+    # If this graph is critical
+    if is_critical == True:
+
+        # Save to critical
+        save('critical', graph6_string,
+             graph6_raw_array_to_string, order, chromatic_number)
 
     # Save the graph
-    save(graph_string, raw_graph_string, order, chromatic_number)
+    save('default', graph6_string,
+         graph6_raw_array_to_string, order, chromatic_number)
+
+    # Log out
+    print(f'{index} out of {len(raw_graphs)} ~ {graph6_raw_array_to_string}')
+
+
+print(f'k={k}')
+print(f'Completed - Created ./graph - {k}')
