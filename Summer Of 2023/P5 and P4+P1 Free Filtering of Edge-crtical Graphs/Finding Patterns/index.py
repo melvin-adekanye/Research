8cf890801b92cea22e1d5115dabd9daee9e10b83
ecclesiastes911 <1baller4god@gmail.com>
2023-05-30 10:24:56 -0600

    Included findign patterns in 5-critical

---
+++ Summer Of 2023/P5 and P4+P1 Free Filtering of Edge-crtical Graphs/Finding Patterns/index.py
@@ -0,0 +1,87 @@
+import collections
+import os
+import shutil
+import numpy
+
+GRAPH_K = 5
+GRPAH6_STRING = []
+SOURCE_PATH = f"../graph-{GRAPH_K}/" + "P4+P1 Free"
+LAYOUT = "circular"
+IMAGE_PATH = f"images/{LAYOUT}/"
+
+
+# Define the path manager
+def path_manager(*paths):
+
+    for path in paths:
+
+        # Check whether the specified path exists or not
+        isExist = os.path.exists(path)
+
+        if not isExist:
+
+            # Create a new directory because it does not exist
+            os.makedirs(path)
+            print(f"The new directory {path} has been created!")
+
+        else:
+
+            # Clear file path for new data
+            shutil.rmtree(path)
+            path_manager(path)
+
+
+# Reading in the files from {SOURCE_PATH}
+print(f"Reading in the files from {SOURCE_PATH}")
+directory = os.fsencode(SOURCE_PATH)
+for file in os.listdir(directory):
+
+    filename = os.fsdecode(file)
+
+    print(f"\n\n====={filename}")
+
+    # Defien a path to the graph (may not actually exist)
+    path = f'{SOURCE_PATH}/{filename}'
+
+    # Try: If the GRAPH_PATH does exist
+    try:
+
+        # Open up and read the file graph
+        f = open(path, "r")
+
+        # FOr every line in the file
+        for line in f:
+
+            # Attain the raw graph numbers
+            string = line.split(None)[0]
+
+            # Append a list to the raw graph
+            GRPAH6_STRING.append(string)
+
+        # CLose the file
+        f.close()
+
+    except:
+
+        # If the graph path doesn't exist skip
+        pass
+
+    print(GRPAH6_STRING)
+
+path_manager(IMAGE_PATH)
+
+# Saving graph images to
+print(f"Saving graphs to {IMAGE_PATH}")
+for (index, string) in enumerate(GRPAH6_STRING):
+
+    G = Graph(string)
+    # print(G.distance_matrix())
+    # print("===============")
+    # print(G.adjacency_matrix())
+    print("\n\n")
+
+
+    p = G.plot(layout=LAYOUT)
+    p.save_image(IMAGE_PATH + str(index) + ".png")
+
+print("Completed ⭐")
