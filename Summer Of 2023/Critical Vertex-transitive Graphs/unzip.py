b07a06dc2d0b3b8c3856a2342bec4cff6edcb129
melvin12345678910 ~ melvin123456789 was taken <melvin.adekanye@intelius.ai>
2023-08-03 11:18:05 -0600

    Partial generation of 5-critical vertex critical graphs

--- Summer Of 2023/Critical Vertex-transitive Graphs/unzip.py
+++ Summer Of 2023/Critical Vertex-transitive Graphs/unzip.py
@@ -17,13 +17,32 @@ for folder in os.listdir(directory):
 
     # Open the sub folder in DATA
     sub_path = f'{SOURCE_PATH}/{sub_folder}'
+    new_sub_path = sub_path
 
-    sub_directory = os.fsencode(sub_path)
+    # Try to rename the file
+    try:
+        new_sub_path = str(sub_path).replace("alltrans", "graph-")
+    except:
+        pass
+
+    os.rename(sub_path, new_sub_path)
+    sub_directory = os.fsencode(new_sub_path)
+
     for file in os.listdir(sub_directory):
 
         filename = os.fsdecode(file)
+        new_filename = filename
+
+        # Try to rename the file
+        try:
+            new_filename = str(filename).replace("alltrans", "graph-")
+        except:
+            pass
 
         new_str = str(sub_directory) + "/" + str(filename)
+        new_filename_str = str(sub_directory) + "/" + str(new_filename)
         new_str = new_str.replace("'", "")[1:]
+        new_filename_str = new_filename_str.replace("'", "")[1:]
 
-        os.system(f'gzip -d "{new_str}"')
+        os.rename(new_str, new_filename_str)
+        os.system(f'gzip -d "{new_filename_str}"')
